b83cf0cc1db09e962523249bb53b83cd7bf51e17
bimo <bimoataullahrahman38@gmail.com>
2025-11-06 16:03:38 +0700

    membuat direktori api endpoint

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,21 @@
-import { login, signup } from './actions'
+//di bawah ini kode halaman login yang dari supabase, 
+// tetapi butuh penyesuaian dengan api endpoint yang sudah direncanain.
 
-export default function LoginPage() {
-  return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
-  )
-}
+//kodenya tetap ada hanya untuk penjelasan implementasi saja, kemudian diubah
+// dan disesuaikan.
+
+// import { login } from '../api/auth/login' -> nanti bukan seperti ini
+// import { signup } from '../api/auth/signup' -> nanti bukan seperti ini 
+
+// export default function LoginPage() {
+//   return (
+//     <form>
+//       <label htmlFor="email">Email:</label>
+//       <input id="email" name="email" type="email" required />
+//       <label htmlFor="password">Password:</label>
+//       <input id="password" name="password" type="password" required />
+//       <button formAction={login}>Log in</button>
+//       <button formAction={signup}>Sign up</button>
+//     </form>
+//   )
+// }
