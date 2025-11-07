9f2b4df252b1507c2ec9c4e62a71e1d0db9debb4
bimo <bimoataullahrahman38@gmail.com>
2025-11-07 08:08:15 +0700

    mengupdate delete account endpoint

--- src/app/api/profile/me/route.ts
+++ src/app/api/profile/me/route.ts
@@ -1,29 +1,59 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers';
+// import client helper (untuk mendapatkan user)
+import { createClient } from '@/utils/supabase/server';
+// import 'createClient' ASLI dari library inti (untuk admin)
+import { createClient as createAdminClient } from '@supabase/supabase-js';
+
 
 export async function DELETE(request: Request) {
-  // 1. Ambil data yang dikirim oleh frontend (payload)
-  //    Contoh: { email: "...", password: "..." }
   try {
     const supabase = await createClient();
+    const {data: {user}, error: authError} = await supabase.auth.getUser();
 
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error("Supabase signout error:", error.message);
-      NextResponse.json(
-        {error: "Gagal untuk logout."},
-        {status: 500} 
+    if(authError || !user){
+      return NextResponse.json(
+        {error: "Tidak terautentikasi"},
+        {status: 401}
       )
     }
+
+    const userIdToDelete = user.id;
+
+    const supabaseAdmin = createAdminClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.SUPABASE_SERVICE_ROLE_KEY!,
+      {
+      // Opsi auth ini PENTING untuk server-side
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false,
+        detectSessionInUrl: false
+        }
+      } 
+    )
+
+    const {error: deleteError} = await supabaseAdmin.auth.admin.deleteUser(userIdToDelete);
+
+    if(deleteError){
+      console.error("Supabase admin delete error:", deleteError.message);
+      return NextResponse.json(
+        {error: "Gagal menghapus akun"},
+        {status: 500}
+      )
+    }
+
+    await supabase.auth.signOut();
+
     NextResponse.json(
-      {message: "Logout berhasil!"},
+      {message: "Akun berhasil dihapus"},
       {status: 200}
     )
   } catch (e) {
-    console.error("Supabase signout error:", e);
-      NextResponse.json(
-        {error: "Gagal untuk logout."},
-        {status: 500} 
+    console.error("Supabase delete error:", e);
+      return NextResponse.json(
+        {error: "Gagal menghapus akun"},
+        {status: 500}
       )
   }
 }
